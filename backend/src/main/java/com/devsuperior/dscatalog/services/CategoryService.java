450f8cc634eaa2f01998e049389101c19d0c7a29
rikelmy-matos <rikelmymatos07@gmail.com>
2025-01-05 23:16:59 -0300

    PageRequest config

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,9 +26,9 @@ public class CategoryService {
 	
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> categoryList = repository.findAll();
-		return categoryList.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> categoryList = repository.findAll(pageRequest);
+		return categoryList.map(x -> new CategoryDTO(x));
 	}
 	
 	@Transactional(readOnly = true)
