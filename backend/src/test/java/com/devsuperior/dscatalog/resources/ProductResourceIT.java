dbadb443bcf9d6a9db204f5b5fccbfc0bc4ad2b2
rikelmy-matos <rikelmymatos07@gmail.com>
2025-04-21 13:21:27 -0300

    Created ProductResourceIT

---
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,98 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+	private ProductDTO dto;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+		nonExistingId = 100L;
+		countTotalProducts = 25L;
+		dto = Factory.createProductDTO();
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+		ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/products?page=0&size=12&sort=name,asc")
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(MockMvcResultMatchers.status().isOk());
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(countTotalProducts));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.content").exists());
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Macbook Pro"));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("PC Gamer"));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.content[2].name").value("PC Gamer Alfa"));	
+	}
+	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(dto);
+		
+		String expectedName = dto.getName();
+		String expectedDescription = dto.getDescription();
+		Double expectedPrice = dto.getPrice();
+		String expectedImgUrl = dto.getImgUrl();
+		Instant expectedDate = dto.getDate();
+		String expectedDateStr = DateTimeFormatter.ISO_INSTANT.format(expectedDate);
+		
+		ResultActions result = mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(MockMvcResultMatchers.status().isOk());
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expectedName));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.description").value(expectedDescription));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.price").value(expectedPrice));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.imgUrl").value(expectedImgUrl));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.date").value(expectedDateStr));
+		result.andExpect(MockMvcResultMatchers.jsonPath("$.categories").exists());
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(dto);
+		ResultActions result = mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", nonExistingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(MockMvcResultMatchers.status().isNotFound());
+	}
+}
