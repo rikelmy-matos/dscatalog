1c1b6ed9163ff300fdc4e8b137769ba5a2c7886f
rikelmy-matos <rikelmymatos07@gmail.com>
2025-05-02 03:03:59 -0300

    new password PUT request

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/PasswordRecoverRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/PasswordRecoverRepository.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscatalog.repositories;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.PasswordRecover;
@@ -8,4 +12,7 @@ import com.devsuperior.dscatalog.entities.PasswordRecover;
 @Repository
 public interface PasswordRecoverRepository extends JpaRepository<PasswordRecover, Long> {
 	
+	@Query("SELECT obj FROM PasswordRecover obj WHERE obj.token = :token AND obj.expiration > :now")
+	List<PasswordRecover> searchValidTokens(String token, Instant now);
+
 }
